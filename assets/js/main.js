d683d05e3200c50f83d8d8419f58b8b2ced3d8db
Siddhesh Shankar <84364874+SiddheshCodeMaster@users.noreply.github.com>
2025-12-08 14:51:41 +0530

    HEADER, FOOTER and HOMEPAGE completed, 1.0.1v

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,15 @@
-document.getElementById("year").innerText = new Date().getFullYear();
+function updateYear() {
+    const span = document.getElementById("year");
+    if (span) {
+        span.textContent = new Date().getFullYear();
+    }
+}
+
+async function importComponent(id, file) {
+            const response = await fetch(file);
+            const html = await response.text();
+            document.getElementById(id).innerHTML = html;
+
+            // return so we can chain .then()
+            return true;
+        }
